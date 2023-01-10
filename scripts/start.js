5c960be61095b06b58c3a6615ddfde3a0ea5b474
HMNConsulting <jake@hmnconsult.com>
2023-01-10 00:41:02 -0500

    fix production var

--- scripts/start.js
+++ scripts/start.js
@@ -1,30 +1,24 @@
-const environment = process.env.NODE_ENV || 'development';
+const concurrently = require('concurrently');
+const upath = require('upath');
 
-console.log('ENV: ' + environment);
+const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
 
-if(environment == 'development'){
-    const concurrently = require('concurrently');
-    const upath = require('upath');
-
-    const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
-
-    concurrently([
-        { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
-        { 
-            command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
-            name: 'SB_BROWSER_SYNC', 
-            prefixColor: 'bgGreen.bold',
-        }
-    ], {
-        prefix: 'name',
-        killOthers: ['failure', 'success'],
-    }).then(success, failure);
-
-    function success() {
-        console.log('Success');    
+concurrently([
+    { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
+    { 
+        command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
+        name: 'SB_BROWSER_SYNC', 
+        prefixColor: 'bgGreen.bold',
     }
+], {
+    prefix: 'name',
+    killOthers: ['failure', 'success'],
+}).then(success, failure);
 
-    function failure() {
-        console.log('Failure');
-    }
+function success() {
+    console.log('Success');    
+}
+
+function failure() {
+    console.log('Failure');
 }
